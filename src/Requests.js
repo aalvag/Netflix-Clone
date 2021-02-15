fadac5a43b3fa2eeef1de46cc096b14b80a5e404
Alejandro David Alvarez <48391594+aalvag@users.noreply.github.com>
2021-02-15 11:03:48 -0300

    fix env

--- src/Requests.js
+++ src/Requests.js
@@ -1,15 +1,14 @@
-const API_KEY = "7eeb056f1aeda94c950ad6b343dd1857";
 const URL = "https://api.themoviedb.org/3";
 
 const requests = {
-  fetchTrending: `${URL}/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `${URL}/discover/tv?api_key=${API_KEY}&with_netwoks=213`,
-  fetchTopRated: `${URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `${URL}/discover/movie?api_key=${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `${URL}/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `${URL}/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `${URL}/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchDocumentaries: `${URL}/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchTrending: `${URL}/trending/all/week?api_key=${process.env.REACT_APP_API_TDMB}&language=en-US`,
+  fetchNetflixOriginals: `${URL}/discover/tv?api_key=${process.env.REACT_APP_API_TDMB}&with_netwoks=213`,
+  fetchTopRated: `${URL}/movie/top_rated?api_key=${process.env.REACT_APP_API_TDMB}&language=en-US`,
+  fetchActionMovies: `${URL}/discover/movie?api_key=${process.env.REACT_APP_API_TDMB}&with_genres=28`,
+  fetchComedyMovies: `${URL}/discover/movie?api_key=${process.env.REACT_APP_API_TDMB}&with_genres=35`,
+  fetchHorrorMovies: `${URL}/discover/movie?api_key=${process.env.REACT_APP_API_TDMB}&with_genres=27`,
+  fetchRomanceMovies: `${URL}/discover/movie?api_key=${process.env.REACT_APP_API_TDMB}&with_genres=10749`,
+  fetchDocumentaries: `${URL}/discover/movie?api_key=${process.env.REACT_APP_API_TDMB}&with_genres=99`,
 };
 
 export default requests;
